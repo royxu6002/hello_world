c702b2bc47a9fddedc27016545d6b87d461dc339
vagrant <vagrant@homestead>
2024-09-17 00:40:38 +0000

    name route

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,14 @@
     <title>
         @yield('title', 'weibo app') - first new application
     </title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </body>
 </html>
