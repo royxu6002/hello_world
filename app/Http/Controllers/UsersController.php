5d60de2889c08161a3a64d138eff75372cd60923
vagrant <vagrant@homestead>
2024-09-17 07:09:41 +0000

    signup page

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,13 +7,18 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
-    public function create()
-    {
-        return view('users.create');
-    }
+    // public function create()
+    // {
+    //     return view('users.create');
+    // }
 
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function create()
+    {
+        return view('users.create');
     }
 }
